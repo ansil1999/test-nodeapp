7515d835f75993b4dcd3859eeaf398e8660945d8
ansil1999 <104411420+ansil1999@users.noreply.github.com>
2023-07-28 17:22:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Sample Node App deployed by ansil\n');
+  res.send('Sample Node App deployed by ansivghnml\n');
 });
 
 app.listen(PORT, HOST);
